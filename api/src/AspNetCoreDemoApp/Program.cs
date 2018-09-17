e2322f1609ec8d8dfdecb441e6d8ad0461bc98ed
Andres Chavez <achav069@fiu.edu>
2018-09-17 08:36:39 -0400

    Add firebase project access

--- api/src/AspNetCoreDemoApp/Program.cs
+++ api/src/AspNetCoreDemoApp/Program.cs
@@ -1,12 +1,24 @@
+using Google.Cloud.Firestore;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System;
 
 namespace AspNetCoreDemoApp
 {
     public class Program
     {
-        public static void Main(string[] args) =>
-            CreateWebHostBuilder(args).Build().Run();
+        public static void InitializeProjectId(string project)
+        {
+            FirestoreDb db = FirestoreDb.Create(project);
+            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", project);
+        }
+        
+           
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            InitializeProjectId("tutorial-959a2");
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
