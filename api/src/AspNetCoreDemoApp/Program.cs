e6ba1f61e6cf9b7a39b146f158e4344ca7c7af5f
Andres Chavez <achav069@fiu.edu>
2018-09-20 13:08:29 -0400

    Builds localhost

--- api/src/AspNetCoreDemoApp/Program.cs
+++ api/src/AspNetCoreDemoApp/Program.cs
@@ -6,18 +6,10 @@ using System;
 namespace AspNetCoreDemoApp
 {
     public class Program
-    {
-        public static void InitializeProjectId(string project)
-        {
-            FirestoreDb db = FirestoreDb.Create(project);
-            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", project);
-        }
-        
-           
+    { 
         public static void Main(string[] args)
         {
-            //CreateWebHostBuilder(args).Build().Run();
-            InitializeProjectId("tutorial-959a2");
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
