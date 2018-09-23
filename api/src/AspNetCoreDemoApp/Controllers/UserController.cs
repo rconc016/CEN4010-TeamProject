2632d0c82f0840523e2c2b56f490638fc395ed39
Andres Chavez <achav069@fiu.edu>
2018-09-23 10:07:28 -0400

    Added User Controller

---
+++ api/src/AspNetCoreDemoApp/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using AspNetCoreDemoApp.Models;
+using AspNetCoreDemoApp.Services;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        /// <summary>
+		/// GET endpoint to retrieve the list of all the users.
+		/// </summary>
+		/// <returns>A list of all <see href="User">s available.</returns>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return userService.FindAll();
+        }
+
+        /// <summary>
+        /// GET endpoint to retrieve a single user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>The user with the corresponding ID.</returns>
+        [HttpGet("{userId}")]
+        public ActionResult<User> Get(string userId)
+        {
+            User user = userService.FindById(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
