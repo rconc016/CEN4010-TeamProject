2ed2f31771871c9920d243bbc158551823091cb7
Andres Chavez <achav069@fiu.edu>
2018-09-24 14:29:52 -0400

    Added arrow function

--- api/src/AspNetCoreDemoApp/Program.cs
+++ api/src/AspNetCoreDemoApp/Program.cs
@@ -7,10 +7,8 @@ namespace AspNetCoreDemoApp
 {
     public class Program
     { 
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateWebHostBuilder(args).Build().Run();
-        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
