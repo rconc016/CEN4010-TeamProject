d9dacf7a93f29ebf89a46c30e78dfd03e9ec8717
Roncon <roncon03@gmail.com>
2018-10-18 14:00:18 -0400

    Added sorting UI to the book browsing page

--- web/src/components/book/book.resolver.ts
+++ web/src/components/book/book.resolver.ts
@@ -1,17 +1,25 @@
-import { Injectable } from "@angular/core";
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
-import { BookService } from "../../services/book/book.service";
-import { Observable } from "rxjs";
-import { BookInterface } from "../book.details/book.interface";
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { BookService } from '../../services/book/book.service';
+import { Observable } from 'rxjs';
+import { BookInterface } from '../book.details/book.interface';
+import { SortCommand } from '../../common/models/sortcommand';
+import { SortDirection } from '../../common/enums/sortdirection';
 
 @Injectable({
     providedIn: 'root'
 })
 export class BookResolver implements Resolve<BookInterface[]> {
+    private readonly sortKey: string;
+
     constructor(private bookService: BookService) {
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<BookInterface[]> {
-        return this.bookService.findAll(null, null, null);
+        let sortCommand = new SortCommand();
+        sortCommand.key = this.sortKey;
+        sortCommand.sortBy = SortDirection.Asc;
+
+        return this.bookService.findAll(sortCommand, null, null);
     }
 }
