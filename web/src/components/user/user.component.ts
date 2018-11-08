da6bfe86c6d4a57c3863054436f026e56a12b552
JackieOLantern <jcast452@fiu.edu>
2018-11-07 21:22:20 -0500

    User can edit email and password, added shipping address and credit card fields

--- web/src/components/user/user.component.ts
+++ web/src/components/user/user.component.ts
@@ -3,8 +3,9 @@ import { UserService } from '../core/user.service';
 import { AuthService } from '../core/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { FirebaseUserModel } from '../core/user.model';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-user',
@@ -14,7 +15,12 @@ import { FirebaseUserModel } from '../core/user.model';
 export class UserComponent implements OnInit {
   user: FirebaseUserModel;
   profileForm: FormGroup;
-
+  successMessageEmail: string = '';
+  errorMessageEmail: string = '';
+  successMessagePassword: string = '';
+  errorMessagePassword: string = '';
+  shippingAddresses: FormArray
+  creditCards: FormArray
 
   constructor(
     private userService: UserService,
@@ -40,8 +46,64 @@ export class UserComponent implements OnInit {
 
   createForm(name) {
     this.profileForm = this.fb.group({
-      name: [name, Validators.required ],
+      name: [name, Validators.required],
+      shippingAddresses: this.fb.array([this.initShippingAddresses()]),
+      creditCard: this.fb.array([this.initCreditCards()])
     });
+  }
+
+  initShippingAddresses() {
+    return this.fb.group({
+      shippingAddress: ['']
+    })
+  }
+
+  initCreditCards() {
+    return this.fb.group({
+      creditCard: ['']
+    })
+  }
+
+  updateEmail(email:string) {
+    var user = firebase.auth().currentUser;
+
+    user.updateEmail(email).then(res => {
+      this.successMessageEmail = "Email was successfully updated";
+      this.errorMessageEmail = "";
+    }).catch(err => {
+      this.successMessageEmail = "";
+      this.errorMessageEmail = "This operation is sensitive and requires recent authentication.Log in again before retrying this request.";
+    });
+  }
+
+  updatePassword(password:string) {
+    var user = firebase.auth().currentUser;
+    var newPassword = password;
+
+    user.updatePassword(newPassword).then(res => {
+      this.successMessagePassword = "Password was successfully updated";
+      this.errorMessagePassword = "";
+    }).catch(err => {
+      this.successMessagePassword = "";
+      this.errorMessagePassword = "This operation is sensitive and requires recent authentication.Log in again before retrying this request.";
+    });
+      
+  }
+
+  addShippingAddress() {
+    this.shippingAddresses = this.profileForm.get('shippingAddresses') as FormArray;
+    this.shippingAddresses.push(this.initShippingAddresses());
+    for (let i = 0; i < this.shippingAddresses.length; i++) {
+      this.user.shippingAddress.push(this.shippingAddresses[i]);
+    }
+  }
+
+  addCreditCard() {
+    this.creditCards = this.profileForm.get('creditCards') as FormArray;
+    this.creditCards.push(this.initCreditCards());
+    for (let i = 0; i < this.creditCards.length; i++) {
+      this.user.creditCards.push(this.creditCards[i]);
+    }
   }
 
   logout(){
@@ -65,7 +127,8 @@ export class UserComponent implements OnInit {
           shippingAddress : data['shippingAddress'],
           provider: this.user.provider,
           image: this.user.image,
-          name: this.user.name
+          name: this.user.name,
+          creditCards: data['creditCards']
         });
   }
 
