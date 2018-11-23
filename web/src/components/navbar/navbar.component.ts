45ca44b4e89a214289adefb9d85ff2e9754fd6e4
JackieOLantern <jcast452@fiu.edu>
2018-11-23 14:50:01 -0500

    Added profile, cart, login/logout routes to navbar

--- web/src/components/navbar/navbar.component.ts
+++ web/src/components/navbar/navbar.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserComponent } from '../user/user.component';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private userComponent: UserComponent) { }
 
   ngOnInit() {
   }
 
+  goHome() {
+    this.router.navigate(['/book']);
+  }
+
+  goProfile() {
+    this.router.navigate(['/user']);
+  }
+
+  goCart() {
+    var user = firebase.auth().currentUser;
+
+    if(user)
+      this.router.navigate(['/cart']);
+    else
+      this.router.navigate(['/login']);
+  }
+
+  goLoginLogout() {
+    var user = firebase.auth().currentUser;
+
+    if (user)
+      this.userComponent.logout();
+    else
+      this.router.navigate(['/login']);
+  }
 }
+
