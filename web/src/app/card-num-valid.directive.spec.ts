668814778bb1565474666c6b13741edcb284f0d8
JackieOLantern <jcast452@fiu.edu>
2018-11-23 14:47:35 -0500

    Created Credit Card Number Validator Directive

---
+++ web/src/app/card-num-valid.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CardNumValidDirective } from './card-num-valid.directive';
+
+describe('CardNumValidDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CardNumValidDirective();
+    expect(directive).toBeTruthy();
+  });
+});
